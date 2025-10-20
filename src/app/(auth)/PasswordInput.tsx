1d54852b2b3cefcdb6660b2a9c409bb2d9d05ae3
ernestluizdiaz <ernestluizdiaz@gmail.com>
2025-10-20 13:30:53 +0800

    change interface into type

--- src/app/(auth)/PasswordInput.tsx
+++ src/app/(auth)/PasswordInput.tsx
@@ -4,8 +4,7 @@ import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Eye, EyeOff } from "lucide-react";
 
-interface PasswordInputProps
-	extends React.InputHTMLAttributes<HTMLInputElement> {}
+type PasswordInputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export function PasswordInput(props: PasswordInputProps) {
 	const [show, setShow] = useState(false);
